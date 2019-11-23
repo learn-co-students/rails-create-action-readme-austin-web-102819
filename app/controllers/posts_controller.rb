f81bc5f5f4cbafea5ac591527e4ebf9fa2f96914
rachfuerst <rach.fuerst@gmail.com>
2019-11-23 11:39:32 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  def create
+    # Create a new post object
+    @post= Post.new
+    # Set the post object's title to the title input in the title field of new.html.erb
+    @post.title= params[:title]
+    # Set the post object's description to that input in the description field of new.html.erb
+    @post.description= params[:description]
+    # Save the object to the database with these attributes
+    @post.save
+    # Redirect the user to the /post/ #show path
+    redirect_to post_path(@post)
+
+  end
+
 end
