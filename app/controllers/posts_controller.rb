1ae56f1a762e8d005e5d12d525494cddc86afb6c
fmwong77 <fongmeiw@gmail.com>
2019-11-24 13:28:36 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,12 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+    @post = new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+
+    redirect_to post_path(@post)
+  end
 end
